de5755fa16e65a6b46071f929b6846dca0188074
Pako Herrera <pakman_198@hotmail.com>
2018-07-09 18:19:32 -0500

    Created Feature component and fixed navigation issue by adding path property to Route components to properly navigate between scenes

--- authentication/client/src/index.js
+++ authentication/client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
@@ -10,6 +10,7 @@ import reducers from './reducers';
 import App from './components/App';
 import Welcome from './components/Welcome';
 import Signup from './components/auth/Signup';
+import Feature from './components/Feature';
 
 const store = createStore(
   reducers,
@@ -21,8 +22,11 @@ ReactDOM.render(
   <Provider store={ store }>
     <BrowserRouter>
       <App>
-        <Route to="/" exact component={Welcome} />
-        <Route to="/signup" component={Signup} />
+        <Switch>
+          <Route path="/signup" component={Signup} />
+          <Route path="/feature" component={Feature} />
+          <Route path="/" exact component={Welcome} />
+        </Switch>
       </App>
     </BrowserRouter>
   </Provider>,
