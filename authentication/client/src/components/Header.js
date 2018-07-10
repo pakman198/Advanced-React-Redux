0528cd2a26553254f0fad7507e4bd8f71488d892
Pako Herrera <pakman_198@hotmail.com>
2018-07-09 19:32:43 -0500

    Added state validation to the header to display proper links when user is signed in/signed out. Added styling to header

--- authentication/client/src/components/Header.js
+++ authentication/client/src/components/Header.js
@@ -1,18 +1,45 @@
+import './HeaderStyle.css';
+
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 class Header extends React.Component {
+  renderLinks(){
+    const { authenticated } = this.props;
+
+    if(authenticated) {
+      return (
+        <div>
+          <Link to="/signout">Sign Out</Link>
+          <Link to="/feature">Feature</Link>
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <Link to="/signup">Sign Up</Link>
+          <Link to="/signin">Sign In</Link>
+        </div>
+      );
+    }
+  }
+
   render() {
+    const links = this.renderLinks();
+
     return (
-      <div>
+      <div className="header">
         <Link to="/">Redux Auth</Link>
-        <Link to="/signup">Sign Up</Link>
-        <Link to="/signin">Sign in</Link>
-        <Link to="/signout">Sign Out</Link>
-        <Link to="/feature">Feature</Link>
+        { links }
       </div>
     )
   }
 }
+function mapstateToProps(state){
+  return {
+    authenticated: state.auth.authenticated
+  };
+}
 
-export default Header;
+export default connect(mapstateToProps)(Header);
